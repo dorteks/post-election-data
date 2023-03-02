ab89cc47b44ed4369af260f2586291ee91b1313b
dorteks <ayodelealayemi@gmail.com>
2023-03-02 18:48:34 +0100

    feat/updated topbar and footbar components

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { Box, VStack, Image, Text, HStack, Stack } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      display="flex"
+      color="#FFFFFF"
+      fontSize="16px"
+      fontStyle="normal"
+      // fontFamily="Jost"
+      lineHeight="23px"
+      bgColor="#4B6587"
+      flexDirection="row"
+      fontWeight="400px"
+      justifyContent="space-between"
+      padding="80px 120px 60px 100px"
+    >
+      <Box width="360px" gap="12px">
+        <Image src="/images/hernalytics-full-logo.svg" alt="hernalytics-logo" />
+        <Stack paddingTop="12px">
+          <Text>Isale Eko Avenue, Dolphin Estate, Ikoyi, Lagos, Nigeria</Text>
+          <Text>© 2022 Hernalytics</Text>
+        </Stack>
+      </Box>
+
+      <VStack alignItems="left" gap="24px">
+        <Text fontWeight="bold">About</Text>
+        <Text>Our Story</Text>
+        <Text>Blog</Text>
+        <Text>About Hernalytics</Text>
+        <Text>Videos</Text>
+      </VStack>
+      <VStack alignItems="left" gap="24px">
+        <Text fontWeight="bold">Support</Text>
+        <Text>FAQs</Text>
+        <Text>Privacy Policy</Text>
+        <Text>Terms of Service</Text>
+      </VStack>
+      <VStack alignItems="left" gap="24px">
+        <Text fontWeight="bold">Let's chat!</Text>
+        <Text>hernalytics@gmail.com</Text>
+        <Text>+234 801 234 5678</Text>
+        <HStack>
+          <Image src="/images/instagram.svg" alt="instagram" />
+          <Image src="/images/twitter.svg" alt="twitter" />
+          <Image src="/images/watsapp.svg" alt="whatsapp" />
+        </HStack>
+      </VStack>
+    </Box>
+  );
 };
 
 export default Footer;
